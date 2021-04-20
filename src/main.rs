015a28c34544f416bd2e11e465643d70804c3d66
The1Penguin <jesper@acorneroftheweb.com>
2021-04-20 19:24:57 +0200

    Updated deps, new string format and new use of args

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,63 @@
 extern crate rofi;
 extern crate regex;
+extern crate clap;
 use regex::Regex;
 use std::path::Path;
 use std::process::Command;
 extern crate glob;
 use glob::glob;
 use std::env;
+use clap::{Arg,App};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-    let entries = generatelist(path);
+
+    let args = App::new("rofi_book")
+	.version("0.1.0")
+	.about("shows all the files in folder and opens using xdg-open")
+	.author("ThePenguin")
+    .args_from_usage("
+        -d, --dir=[TARGET_PATH] 'Sets your target PATH'
+       ")
+	.get_matches();
+
+    let path = args.value_of("dir").unwrap_or(".");
+    let entries = generate_list(path);
     display(path, entries);
     }
 
 fn display(path: &str, dir_entries:std::vec::Vec<std::string::String>){
     match rofi::Rofi::new(&dir_entries).prompt("Books").run() {
-        Ok(_choice) => displayOption(path, &_choice),
+        Ok(_choice) => display_option(path, &_choice),
         Err(rofi::Error::Interrupted) => (),
         Err(e) => println!("Error, {}", e)
     }
 }
 
-fn displayOption(path: &str, file: &str){
-    match isDir(&(path.to_string() + "/" + file)) {
+fn display_option(path: &str, file: &str){
+    match is_dir(&format!("{}/{}", &path, file)) {
         false => {
             Command::new("xdg-open")
-                .args(&[&(path.to_string() + "/" + file)])
+                .args(&[&format!("{}/{}", &path, file)])
                 .spawn()
                 .expect("Failed to start");
             },
         true => {
             let newpath = path.to_string() + "/" + file;
-            display(&newpath, generatelist(&newpath));
+            display(&newpath, generate_list(&newpath));
         },
         }
 }
 
-fn generatelist(path: &str) -> std::vec::Vec<std::string::String>{
+fn generate_list(path: &str) -> std::vec::Vec<std::string::String>{
     let mut temp: Vec<std::string::String> = Vec::new();
     let re = Regex::new(r"^[^_]*/").unwrap();
-    for file in glob(&(path.to_string() + "/*")).expect("Failed to find") {
+    for file in glob(&format!("{}/*", path)).expect("Failed to find") {
         temp.push((re.replace_all(&(file.unwrap().display().to_string()), "")).to_string());
     }
     return temp;
 }
 
-fn isDir(filename: &str) -> bool {
+fn is_dir(filename: &str) -> bool {
     Path::new(filename)
         .is_dir()
 }
